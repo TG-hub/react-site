6da5a3c7fa20523eb9cdfb4675bf3a3acfb57375
Theo Goss <theo@exorttech.co.uk>
2020-09-17 16:29:54 +0100

    create home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import '../../App.css'
+import './home.css'
+import ItemTemplate from '../../components/item-template'
+import ItemList from './items-list.js'
+import {ItemContext} from '../../context/items'
 
 
-function App() {
+
+function Home(props) {
+
+  const item = useContext(ItemContext)
+  
+
   return (
-    <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', height: '100vh', flexDirection: 'column' }}>
-       Home page
+    <div className='Background'>
+      <h1 className='Title'>Welcome to the Store!</h1>
+      <div className='Item-container'>
+        {ItemList.map((items,) => <ItemTemplate image={items.image} title={items.title} price={items.price} onClick={() =>(props.history.push(items.location), item.updateItem(items.location))}/>)}
+      </div>
     </div>
-
   );
 }
 
-export default App;
+export default Home;
---
+++ src/pages/home/items-list.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ItemList = [{image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-12X5.5-IMP-SNARE-DRUM-KENSINGTON-KNIGHT-DRUMAZON_01_1000x.jpg?v=1594060928', title: 'BDC Imp Snare Drum', price: '£379', location:'/item/1'},
+ {image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-14X5.5-AVIATOR-CAST-ALUMINIUM-SNARE-DRUM-DRUMAZON_01_1000x.jpg?v=1594061015', title: 'BDC Aviator Snare Drum', price: '£639', location: '/item/2'},
+ {image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-14X5.5-LEGEND-SE-SNARE-DRUM-SPALTED-BEECH-DRUMAZON_01_1000x.jpg?v=1594061088', title: 'BDC Legend Beech Snare Drum', price: '£665', location: '/item/3'},
+ {image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-14X5.5-MAVERICK-MAPLE-SNARE-DRUM-DRUMAZON_01_1000x.jpg?v=1599848943', title: 'BDC Maverick Snare Drum', price: '£549', location: '/item/4'},
+ {image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-14X6-BLUEBIRD-CHROME-ON-BRASS-SNARE-DRUM-DRUMAZON_01_4dbe97a7-ea81-4031-98af-613f71bf2955_1000x.jpg?v=1599849358', title: 'BDC Bluebird COB Snare Drum', price: '£650', location: '/item/5'},
+ {image: 'https://cdn.shopify.com/s/files/1/1422/9358/products/BRITISH-DRUM-COMPANY-BDC-14X6.5-LOUNGE-SNARE-DRUM-WINDERMERE-PEARL-DRUMAZON_01_1000x.jpg?v=1594062485', title: 'BDC Lounge Snare Drum', price: '£565', location: '/item/6'}]
+
+ export default ItemList
