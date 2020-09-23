7b1e553a137fa9c6e6d99a908d3332977c0b64fa
Theo Goss <theo@exorttech.co.uk>
2020-09-23 12:47:47 +0100

    fix useParams to stop page breaking

--- src/pages/item/index.js
+++ src/pages/item/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import '../../App.css'
 import './item.css'
 import {ItemContext} from '../../context/items'
@@ -9,15 +9,18 @@ import {useParams} from 'react-router-dom'
 
  
 
-const Item = (props) => {
+const Item = () => {
 
-  const itemPage = useContext(ItemContext)
-  const selection = _.find(ItemList, {id: itemPage.item} )
+
   
-  const UrlBackup = () => {
-    const {id} = useParams()
-    return < Item/>
-  } 
+  const itemPage = useContext(ItemContext)
+  const {id} = useParams()
+  const selection = _.find(ItemList, {id: itemPage.item && id} )
+  
+  const [option, setOption] = useState ('')
+  const optionSelect = () => {setOption(option)}
+
+
 
 
   return (
@@ -27,9 +30,9 @@ const Item = (props) => {
         <div className='Item-Details'>
           <p>{selection.title}</p>
           <p>{selection.price}</p>
-            <select>
-              <option value='Red'>Red</option>
-              <option value='Blue'>green</option>
+            <select  onChange={optionSelect}>
+              <option value='One'>One</option>
+              <option value='Two'>Two</option>
             </select>
         </div>
       </div>
