2acfe9931876d17dc01045da05abb62aef681094
Theo Goss <theo@exorttech.co.uk>
2020-09-23 11:53:21 +0100

    attempt to add useParams, not working.

--- src/pages/item/index.js
+++ src/pages/item/index.js
@@ -4,6 +4,7 @@ import './item.css'
 import {ItemContext} from '../../context/items'
 import ItemList from '../home/items-list'
 import _ from 'lodash'
+import {useParams} from 'react-router-dom'
 
 
  
@@ -11,18 +12,24 @@ import _ from 'lodash'
 const Item = (props) => {
 
   const itemPage = useContext(ItemContext)
-  const selection = _.find(ItemList, {id: itemPage} )
+  const selection = _.find(ItemList, {id: itemPage.item} )
+  
+  const UrlBackup = () => {
+    const {id} = useParams()
+    return < Item/>
+  } 
+
 
   return (
     <div className='Container'>
       <div className='Item-Container'>
-        <img src={itemPage.image} width='300' height='300'/>
+        <img src={selection.image} width='300' height='200'/>
         <div className='Item-Details'>
           <p>{selection.title}</p>
           <p>{selection.price}</p>
             <select>
               <option value='Red'>Red</option>
-              <option value='Blue'>Blue</option>
+              <option value='Blue'>green</option>
             </select>
         </div>
       </div>
