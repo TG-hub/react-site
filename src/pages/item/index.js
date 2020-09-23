992ec04fd38443976dfab0cf9b48985c7dbd3700
Theo Goss <theo@exorttech.co.uk>
2020-09-23 17:36:33 +0100

    add random items and add to basket button on item page

--- src/pages/item/index.js
+++ src/pages/item/index.js
@@ -2,42 +2,58 @@ import React, { useContext, useState } from 'react';
 import '../../App.css'
 import './item.css'
 import {ItemContext} from '../../context/items'
+import {BasketContext} from '../../context/basket'
 import ItemList from '../home/items-list'
 import _ from 'lodash'
 import {useParams} from 'react-router-dom'
+import ItemTemplate from '../../components/item-template/item-template'
+
 
 
  
 
-const Item = () => {
+const Item = (props) => {
 
-
+  const basket = useContext(BasketContext)
+  const item = useContext(ItemContext)
   
   const itemPage = useContext(ItemContext)
   const {id} = useParams()
   const selection = _.find(ItemList, {id: itemPage.item && id} )
+  const OtherProduct = _.sampleSize(ItemList, 6)
   
-  const [option, setOption] = useState ('')
-  const optionSelect = () => {setOption(option)}
-
-
+  const [option, setOption] = useState ('One')
+  
 
 
   return (
     <div className='Container'>
-      <div className='Item-Container'>
-        <img src={selection.image} width='300' height='200'/>
+      <div className='Item-Display'>
+        {option === 'One' ? <img className='product-image' src={selection.image} /> : <img className='product-image' src={selection.imageTwo}/> }
         <div className='Item-Details'>
           <p>{selection.title}</p>
           <p>{selection.price}</p>
-            <select  onChange={optionSelect}>
-              <option value='One'>One</option>
-              <option value='Two'>Two</option>
+            <select onChange={(event) => setOption(event.target.value)}>
+              <option value={'One'}>One</option>
+              <option value={'Two'}>Two</option>
             </select>
         </div>
+        <button className='Button' onClick={() => basket.updateBasket(selection)}>Add to Basket</button>
+      </div>
+      <div className='Other-Products'>
+      {OtherProduct.map((items) => <ItemTemplate image={items.image} title={items.title} price={items.price} onClick={() =>{
+        props.history.push(`/item/${items.id}`)
+        item.updateItem(items.id)}}  />)}
       </div>
     </div>
   );
 }
 
 export default Item;
+/*<div className='Item-container'>
+        
+        
+        {ItemList.map((items) => <ItemTemplate image={items.image} title={items.title} price={items.price} onClick={() =>{
+        props.history.push(`/item/${items.id}`)
+        item.updateItem(items.id)}}  />)}
+      </div>*/
